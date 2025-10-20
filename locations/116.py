caccfe211b497d206dfb9f0c2eb34e80fa053e86
Luka <akrus.001@mail.ru>
2025-10-20 12:38:41 +0300

    Лука 116

--- locations/116.py
+++ locations/116.py
@@ -1,1 +1,27 @@
-#ЭТО ФАЙЛ ЛУКИ!!!!!
+from telebot import types
+
+
+def user_enters_location(bot, user, location, all_users):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(types.KeyboardButton(text="Поиграть на пианино"))
+    keyboard.add(types.KeyboardButton(text="Потыкать по доске"))
+    bot.send_message(user['id'], 'Вы в каб. 116', reply_markup=keyboard)
+
+
+def user_leaves_location(bot, user, location, all_users):
+    bot.send_message(user['id'], 'Вы покинули каб. 116')
+
+
+def user_message(bot, message, user, location, all_users):
+    if message == 'Поиграть на пианино':
+        user['energy'] = min(100, user['energy'] + 5)
+        bot.send_message(user['id'], f'Вы поиграли на пианино'
+                                     f'У вас ничего не изменилось')
+    elif message == 'Потыкать по доске':
+        user['energy'] = min(100, user['energy'] + 5)
+        user['experience'] = min(100, user['experience'] + 5)
+        bot.send_message(user['id'], f'Вы поиграли на пианино'
+                                     f'У вас теперь {user['energy']} энергии и {user['experience']} опыта :)')
+
+    else:
+        bot.send_message(user['id'], 'Я вас не понял :(')
