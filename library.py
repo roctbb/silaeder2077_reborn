555631acad27dd09e0afdca60f6bbbf230f1817a
Luka <akrus.001@mail.ru>
2025-10-20 13:12:33 +0300

    Лука 116

--- library.py
+++ library.py
@@ -10,8 +10,13 @@ locations = [
     {
         "id": "yard",
         "name": 'двор',
+        "inventory": [],
+    },
+    {
+        "id": "116",
+        "name": 'каб. 116',
         "inventory": []
-    }
+             }
 ]
 
 modules = {
