f1cab5765bbce94c9e6b7fa714acd3caa156bed3
Luka <akrus.001@mail.ru>
2025-10-21 12:43:59 +0300

    Лука 116

--- locations/room_116.py
+++ locations/room_116.py
@@ -1,4 +1,5 @@
 from telebot import types
+import random
 
 
 def user_enters_location(bot, user, location, all_users):
@@ -14,14 +15,17 @@ def user_leaves_location(bot, user, location, all_users):
 
 def user_message(bot, message, user, location, all_users):
     if message == 'Поиграть на пианино':
-        user['energy'] = min(100, user['energy'] + 5)
-        bot.send_message(user['id'], f'Вы поиграли на пианино'
-                                     f'У вас ничего не изменилось')
-    elif message == 'Потыкать по доске':
-        user['energy'] = min(100, user['energy'] + 5)
+        user['energy'] = min(100, user['energy'] - 5)
         user['experience'] = min(100, user['experience'] + 5)
         bot.send_message(user['id'], f'Вы поиграли на пианино'
-                                     f'У вас теперь {user['energy']} энергии и {user['experience']} опыта :)')
+                                     f'Теперь у вас {user['experience']} опыта и {user['energy']} энергии')
+    elif message == 'Потыкать по доске':
+        user['energy'] = min(100, user['energy'] - 5)
+        bot.send_message(user['id'], f'Вы поиграли на пианино'
+                                     f'У вас теперь {user['energy']} энергии, но у вас поднялосб настроение')
+        if random.randint(0, 3) == 1:
+            bot.send_message(user['id'], f'Вас спалил учитель!!!'
+                                         f'И отвели в 105...')
 
     else:
         bot.send_message(user['id'], 'Я вас не понял :(')
