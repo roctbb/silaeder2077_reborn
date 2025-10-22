1330674324fa25e09df11221bf0fcb21c282c87b
aleks <aleksander.goosev@gmail.com>
2025-10-22 11:11:49 +0300

    Я типо Саня

--- library.py
+++ library.py
@@ -2,9 +2,8 @@ from config import TOKEN
 import telebot
 import locations.yard as yard
 import locations.gym as gym
-import locations.UnderTheCarpet as UnderTheCarpet
 import locations.hall as hall
-import locations.room_116 as room_116
+import locations.toilet as toilet
 bot = telebot.TeleBot(TOKEN)
 
 users = []
@@ -24,28 +23,22 @@ locations = [
         "id": "hall",
         "name": 'холл',
         "inventory": []
-    }, 
-    {
-        "id": "UnderTheCarpet",
-        "name": 'комната под ковром',
     },
     {
-        "id": "room_116",
+        "id": "116",
         "name": 'каб. 116',
+        "inventory": []
+    },
+    {
+        "id": "toilet",
+        "name": 'туалет',
         "inventory": []
     }
 ]
 
-def getLocList():
-    keys = []
-    for i in locations:
-        keys.append(i['id'])
-    return keys
-
 modules = {
     'yard': yard,
     'gym': gym,
-    'UnderTheCarpet': UnderTheCarpet, 
     'hall': hall,
-    'room_116' : room_116
+    'toilet': toilet
 }
