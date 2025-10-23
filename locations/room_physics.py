55aff219956efdf8f1aa58d1d5dd9f6dc2c9a687
Luka <akrus.001@mail.ru>
2025-10-23 11:06:08 +0300

    Лука 116 и физика

--- locations/room_physics.py
+++ locations/room_physics.py
@@ -35,30 +35,10 @@ def user_message(bot, message, user, location, all_users):
         user['energy'] -= 5
         bot.send_message(user['id'], f'Вы порылись в коробке и нашли несколько интересных вещей\n'
                                  f'У вас теперь {user["energy"]} энергии, но у вас поднялось настроение')
-        bot.send_message(user['id'], f"Вы заметили что на ноутбуке рядом открыт дневник. Можно изменить себе оценки")
-        if random.randint(0, 3) == 1:
-            bot.send_message(user['id'], f'Вас спалил учитель!!!'
-                                         f'И отвели в 105...')
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(types.KeyboardButton(text="Изменить оценки"))
-        keyboard.add(types.KeyboardButton(text="Не менять оценки"))
-        keyboard.add(types.KeyboardButton(text="Перейти во холл"))
-        bot.send_message(user['id'], 'Что будете делать?', reply_markup=keyboard)
-
-
 
                 #bot.send_message(user['id'], f'Перейти в каб. 105')
-    elif message == "Изменить оценки":
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(types.KeyboardButton(text="Перейти в каб. 105"))
-        bot.send_message(user['id'], 'Что у вас за мысли?\nВ 105!!!!!!', reply_markup=keyboard)
-    elif message == "Не менять оценки":
-        bot.send_message(user['id'], "Ок. Вы хороший ученик. Вас не отправят в 105 :)")
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(types.KeyboardButton(text="Поиграть на пианино"))
-        keyboard.add(types.KeyboardButton(text="Потыкать по доске"))
-        keyboard.add(types.KeyboardButton(text="Перейти во холл"))
-        bot.send_message(user['id'], 'Вы в каб. 116', reply_markup=keyboard)
+    elif message == "Попросить конфетку":
+        bot.send_message(user['id'], '0о\n-')
 
 
     else:
