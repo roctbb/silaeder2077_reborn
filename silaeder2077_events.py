5978c742a04a9a2481e70650f0e5903217f6a66b
roctbb <roctbb@gmail.com>
2025-10-23 11:14:19 +0300

    Добавил функцию common_events и оптимизировал вызов событий для пользователей в локациях

--- silaeder2077_events.py
+++ silaeder2077_events.py
@@ -2,12 +2,23 @@ import time
 
 from methods import *
 
+
+def common_events(bot, user):
+    if random.randint(1, 5) == 1:
+        user['food'] = max(0, user['energy'] - 1)
+    if random.randint(1, 5) == 1:
+        user['water'] = max(0, user['water'] - 1)
+
+
 while True:
+    for user in users:
+        common_events(bot, user)
+
     for location in locations:
-        users = get_location_users(location['id'])
+        location_users = get_location_users(location['id'])
 
         try:
-            modules[location['id']].run_events(bot, location, users)
+            modules[location['id']].run_events(bot, location, location_users)
         except Exception as e:
             print(e)
 
