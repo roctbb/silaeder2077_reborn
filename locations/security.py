94b3ee48487e72ab3b642be14619f14702665f83
klim <klimchanske1@gmail.com>
2025-10-24 11:31:06 +0300

    охрана

--- locations/security.py
+++ locations/security.py
@@ -20,33 +20,34 @@ def user_message(bot, message, user, location, all_users):
         users.remove(user)
         bot.send_message(user['id'], f'Вы умерли.')
         return
-    keyboard.add(types.KeyboardButton(text="Взять броню"))
-    keyboard.add(types.KeyboardButton(text="Взять оружие"))
-    keyboard.add(types.KeyboardButton(text="попить странную жижу в стакане"))
-    if message == 'Взять броню':
-        if 'armor' in location['inventory']:
-            location['inventory'].remove('armor')
-            user['inventory'].append('armor')
-    if message == 'Взять оружие':
-        if 'gun' in location['inventory']:
-            location['inventory'].remove('gun')
-            user['inventory'].append('gun')
-    if message == 'попить странную жижу в стакане':
-        user['energy'] += 250
-        if random.randint(0, 3) == 1:
-            if 'armor' in user['inventory']:
-                bot.send_message(user['id'], f'Вас спалил учитель!!!')
-                bot.send_message(user['id'], f'По вам начали стрелять')
-                bot.send_message(user['id'], f'вы чудом спаслись')
-                user['inventory'].remove('armor')
-            elif 'gun' in user['inventory']:
-                bot.send_message(user['id'], f'Вас спалил учитель!!!')
-                bot.send_message(user['id'], f'Вы отстрелялись  на время')
-                bot.send_message(user['id'], f'вы чудом спаслись')
-                user['inventory'].remove('gun')
-            else:
-                bot.send_message(user['id'], f'Вас спалил учитель!!!')
-                bot.send_message(user['id'], f'Время написать объяснительную')
+    elif message == 'Зайти и что нибудь сделать':
+        keyboard.add(types.KeyboardButton(text="Взять броню"))
+        keyboard.add(types.KeyboardButton(text="Взять оружие"))
+        keyboard.add(types.KeyboardButton(text="попить странную жижу в стакане"))
+        if message == 'Взять броню':
+            if 'armor' in location['inventory']:
+                location['inventory'].remove('armor')
+                user['inventory'].append('armor')
+        if message == 'Взять оружие':
+            if 'gun' in location['inventory']:
+                location['inventory'].remove('gun')
+                user['inventory'].append('gun')
+        if message == 'попить странную жижу в стакане':
+            user['energy'] += 250
+            if random.randint(0, 3) == 1:
+                if 'armor' in user['inventory']:
+                    bot.send_message(user['id'], f'Вас спалил учитель!!!')
+                    bot.send_message(user['id'], f'По вам начали стрелять')
+                    bot.send_message(user['id'], f'вы чудом спаслись')
+                    user['inventory'].remove('armor')
+                elif 'gun' in user['inventory']:
+                    bot.send_message(user['id'], f'Вас спалил учитель!!!')
+                    bot.send_message(user['id'], f'Вы отстрелялись на время')
+                    bot.send_message(user['id'], f'вы чудом спаслись')
+                    user['inventory'].remove('gun')
+                else:
+                    bot.send_message(user['id'], f'Вас спалил учитель!!!')
+                    bot.send_message(user['id'], f'Время написать объяснительную')
 
     else:
         bot.send_message(user['id'], 'Я вас не понял :(')
